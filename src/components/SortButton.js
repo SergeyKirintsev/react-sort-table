26bba99b1519751ca3674b9e9251d0bf9a398821
SergeyKirintsev <kserg80@gmail.com>
2021-04-04 11:32:40 +0300

    gh-pages

--- src/components/SortButton.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SortButton = ({ handleClick, title }) => (
-  <button onClick={handleClick} className="btn btn-primary age">
-    {title}
-  </button>
-);
-
-export default SortButton;
